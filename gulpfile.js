d90104460466589749a920f113267be9181e9739
Craig Taub <craigtaub@gmail.com>
2016-08-11 18:42:42 +0100

    added watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var babelify = require('babelify');
 var browserify = require('browserify');
+var watchify = require('watchify');
 var buffer = require('vinyl-buffer');
 var gulp = require('gulp');
 var envify = require('envify/custom');
@@ -23,22 +24,53 @@ var files = {
 
 require('babel-register');
 
-gulp.task('browserify', function() {
+// taken from https://blog.avisi.nl/2014/04/25/how-to-keep-a-fast-build-with-browserify-and-reactjs/
+function bundleJs(watch) {
     var options = {
         entries: [files.js.entryPoints.client],
-        extensions: ['.js', '.jsx']
+        extensions: ['.js', '.jsx'],
+        cache: {}, // required for watchify
+        packageCache: {}, // required for watchify
+        fullPaths: watch // required to be true only for watchify
     };
-    return browserify(options)
-        .plugin('bundle-collapser/plugin')
-        .transform({global: true}, envify({
-            _: 'purge',
-            NODE_ENV: 'development'
-        }))
+    var bundler = watch ? watchify(browserify(options)) : browserify(options);
+    bundler
         .transform(babelify, {
-            presets: ['es2015', 'react']
-        })
-        .bundle()
-        .pipe(source('bundle.min.js'))
-        .pipe(buffer())
-        .pipe(gulp.dest(files.staticDest.js.app))
-});
+           presets: ['es2015', 'react']
+       })
+       // think extra transform just deals with envify.
+       .transform(envify({
+           // replaces environment checks with strings..
+           // e.g. process.env.NODE_ENV becomes 'development'..
+           _: 'purge',
+           NODE_ENV: 'development'
+       }));
+
+    function rebundleJs(done) {
+        return bundler
+            .plugin('bundle-collapser/plugin') //saves bytes in bundle. collapses paths.
+            .bundle() // the actual thing that bundles
+            .on('error', function(error) {
+                // error
+                console.log('error:' , error.message);
+                // done();
+            })
+            .pipe(source('bundle.min.js'))
+            .pipe(buffer()) // converts stream to buffer..otherwise file itself is emitted
+            // basically allows us to control destination better
+            .pipe(gulp.dest(files.staticDest.js.app))
+            .on('end', function() {
+               // ended
+               console.log('rebundled');
+            });
+    }
+    bundler.on('update', rebundleJs); // watchify emits 'update' when file changes..generate new bundle.
+    // rebundling is faster after first one due to caching.
+    // faster feedback cycle.
+
+    return rebundleJs;
+}
+
+gulp.task('browserify', bundleJs(false));
+
+gulp.task('watchify', bundleJs(true));
